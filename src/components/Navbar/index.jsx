89daa1dd6443a3ae9c3bdd84e979e2bf624c5163
Gabriel Campolina <gcampolinap@gmail.com>
2025-05-22 01:35:35 -0300

    more changes for front-end

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -20,18 +20,6 @@ export default function NavBar() {
 
 
         <div className="end-nav">
-          <div className="nav-search">
-            <input
-              type="text"
-              placeholder="Buscar por um jogo.."
-              className="search-input"
-              value={busca}
-              onChange={(e) => setBusca(e.target.value)}
-            />
-            <button className="search-button">
-              <i className="fa-solid fa-magnifying-glass"></i>
-            </button>
-          </div>
 
           <Link className="btnEntrar" to="/login">
              Entrar <i className="fa-solid fa-right-to-bracket"></i>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,32 +2,40 @@ import "./style.css";
 import { Link } from "react-router-dom";
 import jogos from "../../components/Jogos/jogos.js";
 import { useBusca } from "../../hooks/BuscaContext.jsx";
-
-
+import { useEffect } from "react";
 
 export default function Home() {
-    const { busca } = useBusca();
+  const { busca, setBusca } = useBusca();
+  useEffect(() => {
+    // limpa a busca ao entrar na home
+    setBusca("");
+  }, []);
 
   const jogosFiltrados = jogos.filter((game) =>
     game.nome.toLowerCase().includes(busca.toLowerCase())
   );
-  
+
   return (
     <div className="home-container">
+
+      <input
+        type="text"
+        placeholder="Buscar por um jogo..."
+        value={busca}
+        onChange={(e) => setBusca(e.target.value)}
+        className="search-input"
+      />
+
+
       <div className="game-grid">
         {jogosFiltrados.map((game) => (
-
-          <Link to={`/jogo/${game.id}`} key={game.id} className="game-card"> 
-          
-            
+          <Link to={`/jogo/${game.id}`} key={game.id} className="game-card">
             <img src={game.imagem} alt={game.nome} />
             <div className="info-card">
               <h2>{game.nome}</h2>
               <p>{game.ano}</p>
             </div>
-         
-          </Link> 
-          
+          </Link>
         ))}
       </div>
     </div>
