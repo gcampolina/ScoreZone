ae4f83c6ce29eaaa4954d646995038b94ba0d21f
Gabriel Campolina <gcampolinap@gmail.com>
2025-05-28 00:55:07 -0300

    user registration added

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -7,15 +7,79 @@ import axios from 'axios';
 export default function Login({ setIsAuthenticated }) {
 
   const [usuario, setUsuario] = useState("");
-  const [senha, setSenha] = useState("");
+  const [senhaLogin, setSenhaLogin] = useState("");
+  const [senhaCadastro, setSenhaCadastro] = useState("");
   const [erroLogin, setErroLogin] = useState("");
+  const [sucess, setSucess] = useState("");
   const navigate = useNavigate();
+  const [mostrarCadastro, setMostrarCadastro] = useState(false);
+  const [email, setEmail] = useState("");
+  const [nome, setNome] = useState("");
+  const [idade, setIdade] = useState("");
+  
+
+
+  async function handleCadastro(e) {
+  e.preventDefault();
+  setErroLogin("");
+  setSucess("");
+
+if (!email.match(/^[^@]+@[^@]+\.[^@]+$/)) {
+  setErroLogin("Email inválido. Use um formato como nome@dominio.com");
+  setTimeout(() => {
+        setErroLogin("");
+      }, 2000);
+  return;
+}
+
+  try {
+    const response = await axios.post('http://localhost:3000/usuarios', {
+      nome,
+      email,
+      senha: senhaCadastro,
+      idade: idade ? parseInt(idade, 10) : null,
+    });
+
+    if (response.status === 201) {
+      setSucess("Cadastro realizado com sucesso!");
+      setMostrarCadastro(false); // volta pro login
+      setTimeout(() => {
+        setSucess("");
+      }, 2000);
+    }
+  } catch (error) {
+    if (error.response) {
+      const { status, data } = error.response;
+
+      if (status === 409) {
+      setErroLogin('Email já cadastrado.');
+    } else if (data.error) {
+      setErroLogin('Erro ao cadastrar: ' + data.error);
+    } else if (data.message) {
+      setErroLogin('Erro ao cadastrar: ' + data.message);
+    } else {
+      setErroLogin('Erro ao cadastrar.');
+    }
+  } else {
+    setErroLogin('Erro inesperado. Tente novamente.');
+  }
+
+    setTimeout(() => {
+    setErroLogin('');
+  }, 3000);
+
+
+    console.error(error);
+  }
+}
+
+  
 
   async function handleLogin(e) {
     e.preventDefault();
     setErroLogin("");
     try {
-      const response = await axios.post('http://localhost:3000/login', {email: usuario, senha });
+      const response = await axios.post('http://localhost:3000/login', {email: usuario, senha: senhaLogin });
 
       if (response.status === 200) {
         localStorage.setItem('token', response.data.token);
@@ -51,23 +115,67 @@ export default function Login({ setIsAuthenticated }) {
 }
 
   return (
-<>
-<div className="erroContainer">
-{erroLogin && <div className="erro-login">{erroLogin}</div>}
-</div>
+    <>
 
-   <div className="login-container">
-      <h1>Login</h1>
-      <form className="formLogin" onSubmit={(e) => e.preventDefault()}>
-        <input type="text" placeholder="Email" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
-        <input type="password" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)}/>
-        <button type="button" onClick={handleLogin}>Entrar</button>
-      </form>
-    </div>
-   
-   
-</>
+      <div className="erroContainer">
+        {sucess && <div className="sucess-login">{sucess}</div>}
+      </div>
+      <div className="erroContainer">
+        {erroLogin && <div className="erro-login">{erroLogin}</div>}
+      </div>
 
+      <div className="login-container">
+        <h1>{mostrarCadastro ? 'Cadastro' : 'Login'}</h1>
+
+        {!mostrarCadastro ? (
+          <form className="formLogin" onSubmit={handleLogin}>
+            <input
+              type="text"
+              placeholder="Email"
+              value={usuario}
+              onChange={(e) => setUsuario(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Senha"
+              value={senhaLogin}
+              onChange={(e) => setSenhaLogin(e.target.value)}
+            />
+            <button type="submit">Entrar</button>
+            <p>Não possui uma conta? <a href="#" onClick={e => { e.preventDefault(); setMostrarCadastro(true)}}>Cadastrar-se agora!</a></p>
+          </form>
+        ) : (
+          <form className="formLogin" onSubmit={handleCadastro}>
+            <input
+              type="text"
+              placeholder="Nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Idade"
+              value={idade}
+              onChange={(e) => setIdade(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Senha"
+              value={senhaCadastro}
+              onChange={(e) => setSenhaCadastro(e.target.value)}
+            />
+            <button type="submit">Cadastrar</button>
+            <p>Já tem uma conta? <a href="#" onClick={() => setMostrarCadastro(false)}>Entrar agora!</a></p>
+          </form>
+        )}
+      </div>
+    </>
   );
 }
 
